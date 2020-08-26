17f7523812475e105512d83974fdf078b8a806b3
Mauro Rey <rey.mauro.g@gmail.com>
2020-08-26 03:42:02 -0300

    rosalind 11 done

--- Rosalind/11-15/11-15.py
+++ Rosalind/11-15/11-15.py
@@ -13,54 +13,54 @@ rosalind_dir =  '/media/pulpo/DB/bioinf/scriptsPy/Rosalind/11-15/'
 text_file_hamm = 'rosalind_cons.txt'
 full_directory = os.path.join(rosalind_dir, text_file_hamm)
 
-fh = (open(full_directory)).read()
+fh = (open(full_directory))
 
-len_seq = 0
-consensus = ''
 
-# creo el diccionario de bases, con un largo igual a las secuencias
+
+sequences = []	
+temp_line = ''
+for lines in fh:
+	if (lines.startswith('>') is False):
+		temp_line += lines.rstrip()
+	if (temp_line != '' and lines.startswith('>')):
+		sequences.append(temp_line)
+		temp_line = ''
+sequences.append(temp_line)
+del temp_line
+
+len_seq = len(sequences[0])
+
 profile = {
-			'A': [],
-			'C': [],
-			'G': [],
-			'T': []
+	'A': [],
+	'C': [],
+	'G': [],
+	'T': []
 }
 
-for lines in fh:
-	print(lines)
-	if re.findall('^[^>]', lines): continue
-	len_seq = len(lines)
-	break
 
-
-for n in range(len_seq-1): 
+for n in range(len_seq): 
 	profile['A'].append(0)
 	profile['C'].append(0)
 	profile['G'].append(0)
 	profile['T'].append(0)
 
-	
-'''
-for lines in fh:
 
-	print(type(lines))
 
-	if not lines.startswith('>'):
-		for j, nit_bas in enumerate(lines):
-			print(nit_bas)
-			if nit_bas == '\n':break
-			profile[nit_bas][j] += 1
+for seq in sequences:
+	for i, nit_base in enumerate(seq):
+		profile[nit_base][i] += 1
 
-		#print(int((i-1)/2))'''
+
 
 def return_nit_bas(bases):
 	bases.sort(key = lambda x: x[0], reverse = True)
+	print (bases)
 	return bases[0][1]
 
 
 
-
-for i in range(len_seq-1):
+consensus = ''
+for i in range(len_seq):
 	consensus += return_nit_bas([[profile['A'][i], 'A'], [profile['C'][i], 'C'],
 	[profile['G'][i], 'G'], [profile['T'][i], 'T']])
 
@@ -69,6 +69,7 @@ print(consensus)
 
 for k, v in profile.items():
 	print(k, end = ': ')
-	for i in profile[k]:
+	for i in range(len_seq):
 		print(profile[k][i], end = ' ')
 	print('\n')
+    
